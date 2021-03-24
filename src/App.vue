8278ca913a84a05eb397d36d192b0e962372fe0b
UntillNIGHT <lengleng9090@gmail.com>
2021-03-24 12:26:54 +0700

    [UPDATE]App.vue test color

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
   <img alt="Vue logo" class="justify-self-center" src="./assets/logo.png">
   </div>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p class="text-rose-800">This is text custom color</p>
+  <p class="text-salmon-dark">This is text custom color</p>
 </template>
 
 <script>
