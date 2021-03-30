c7bc118773bcd65ab0efba5b07b4bc6b558131cd
UntillNIGHT <lengleng9090@gmail.com>
2021-03-30 20:16:13 +0700

    [UPDATE]Challenge 3 component

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@
     <div class="w-full h-full md:w-1/2">
       <div class="container">
         <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+        <BaseCard>
         <HelloWorld msg="Welcome to VueCLI project" />
+        </BaseCard>
       </div>
 
       <div class="container">
         <div class="survey-container">
           <form @submit.prevent="submitForm">
-            <div>
+            <BaseCard>
               <h2 class="heading">
                 How was your Vue.js learning experience?
               </h2>
@@ -28,9 +30,9 @@
               <p v-if="invalidNameInput" class="text-red-500">
                 Please enter your name!
               </p>
-            </div>
+            </BaseCard>
 
-            <div>
+            <BaseCard>
               <h2 class="heading">My learning experience was ...</h2>
 
               <div>
@@ -68,7 +70,7 @@
               <p v-if="invalidRatingInput" class="text-red-500">
                 Please choose your learning experience!
               </p>
-            </div>
+            </BaseCard>
             <button class="btn">
               Submit
             </button>
@@ -80,11 +82,13 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import BaseCard from './components/BaseCard.vue';
 
 export default {
   name: 'App',
   components: {
+    BaseCard,
     HelloWorld
   },
   data() {
