b24439af562aa8aa3a789a4fe46cf814b13a4b3a
UntillNIGHT <lengleng9090@gmail.com>
2021-03-30 19:54:36 +0700

    [UPDATE]Challenge 2 form submit

--- src/App.vue
+++ src/App.vue
@@ -8,30 +8,32 @@
 
       <div class="container">
         <div class="survey-container">
-          <form action="">
+          <form @submit.prevent="submitForm">
             <div>
+              <p v-if="invalidNameInput" class="bg-salmon-darkest">Please enter your name!</p>
               <h2 class="heading">
                 How was your Vue.js learning experience?
               </h2>
               <label class="label" for="name">Your Name</label>
-              <input class="input" id="name" type="text" />
+              <input class="input" id="name" type="text" v-model="enteredName" />
             </div>
 
             <div>
+              <p v-if="invalidRatingInput" class="bg-salmon-darkest">Please choose your learning experience!</p>
               <h2 class="heading">My learning experience was ...</h2>
 
               <div>
-                <input type="radio" name="rating" id="rating-poor" />
+                <input type="radio" name="rating" id="rating-poor" v-model="rating" />
                 <label class="label" for="rating-poor">Poor</label>
               </div>
 
               <div>
-                <input type="radio" name="rating" id="rating-avg" />
+                <input type="radio" name="rating" id="rating-avg" v-model="rating" />
                 <label class="label" for="rating-avg">Average</label>
               </div>
 
               <div>
-                <input type="radio" name="rating" id="rating-great" />
+                <input type="radio" name="rating" id="rating-great" v-model="rating" />
                 <label class="label" for="rating-great">Great</label>
               </div>
             </div>
@@ -52,6 +54,24 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+   data() {
+    return {
+      enteredName: '',
+      rating: null,
+      invalidNameInput: false,
+      invalidRatingInput: false
+    }
+  },
+  methods:{
+    submitForm(){
+      this.invalidNameInput = (this.enteredName === '' ? true : false);
+      this.invalidRatingInput = (this.rating === null ? true : false);
+      console.log("invalidNameInput : "+this.invalidNameInput);
+      console.log("name : "+this.enteredName);
+      console.log("invalidRatingInput : "+this.invalidRatingInput);
+      console.log("rating : "+this.rating);
+    }
   }
 }
 </script>
